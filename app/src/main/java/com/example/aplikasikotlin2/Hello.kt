404c9224bd27820e1abe4b6c7ed0240399121dd5
selena2355 <hanifahalya771@gmail.com>
2024-02-24 13:14:29 +0700

    init akhir

--- app/src/main/java/com/example/aplikasikotlin2/Hello.kt
+++ app/src/main/java/com/example/aplikasikotlin2/Hello.kt
@@ -1,57 +1,45 @@
 package com.example.aplikasikotlin2
 
-fun biodata(): Array<String> {
-    val nama = "Hanifah Alya Nuraini"
-    val ttl = "Ponorogo, 23 Mei 2005"
-    val alamat = "Sukosari, Babadan, Ponorogo"
-    return arrayOf(nama, ttl, alamat)
+fun biodt(): Array<String>{
+    val nm = "Hanifah Alya Nuraini"
+    val ttl= "Ponorogo, 23 Mei 2005"
+    val almt= "Sukosari, Babadan, Ponorogo"
+    return arrayOf(nm,ttl,almt)
 }
 
-fun hobi(): List<String> {
-    return listOf("Membaca", "Menulis", "Menggambar")
+fun hb(): Array<String>{
+    val bc = """
+        Membaca
+                             Ketika saya membaca novel atau komik,
+                             saya merasa sedang berada di dunia itu. Otak saya memvisualisasikannya.
+    """.trimIndent()
+    val tls = """
+        Menulis
+                             Menulis membantu saya mencurahkan imajinasi saya,
+                             menuliskan ide-ide yang muncul seketika.
+    """.trimIndent()
+    val gbr = """
+        Menggambar
+                             Menggambar juga membantu saya mencurahkan imajinasi saya,
+                             memvisualisasikannya di atas kertas. Bukan lagi di otak.
+    """.trimIndent()
+    return arrayOf(bc, tls, gbr)
 }
 
-fun hobiDeskripsi(): Array<Pair<String, String>> {
-    return arrayOf(
-        Pair("Membaca", """
-                Ketika saya membaca novel atau komik,
-                saya merasa sedang berada di dunia itu. Otak saya memvisualisasikannya.
-            """
-        ),
-        Pair("Menulis", """
-                Menulis membantu saya mencurahkan imajinasi saya,
-                menuliskan ide-ide yang muncul seketika.
-            """
-        ),
-        Pair("Menggambar", """
-                Menggambar juga membantu saya mencurahkan imajinasi saya,
-                memvisualisasikannya di atas kertas. Bukan lagi di otak.
-            """
-        )
-    )
+fun cetak(){
+    println("=============================")
+    println("------*(Biodata Saya)*-------")
+    println("=============================")
+    println("Nama   : ${biodt()[0]}")
+    println("Usia   : ${biodt()[1]}")
+    println("Alamat : ${biodt()[2]}")
+    println("""
+    Hobi   : ${hb()[0]}
+             ${hb()[1]}
+             ${hb()[2]}
+    """.trimIndent())
 }
 
-fun cetakBiodata(biodata: Array<String>, hobi: List<String>) {
-    println("==================")
-    println("===Biodata Saya===")
-    println("==================")
-    println("Nama   : ${biodata[0]}")
-    println("Usia   : ${biodata[1]}")
-    println("Alamat : ${biodata[2]}")
-    println("Hobi:")
-    val dataHobiDeskripsi = hobiDeskripsi()
-    for ((hobi, deskripsi) in dataHobiDeskripsi) {
-        println("""
-            ${hobi}
-              ${deskripsi}
-        """.trimIndent())
-    }
-}
-
-
-
-fun main() {
-    val dataBiodata = biodata()
-    val dataHobi = hobi()
-    cetakBiodata(dataBiodata, dataHobi)
+fun main(){
+    cetak()
 }
